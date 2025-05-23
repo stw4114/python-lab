2259ff95c9e827500371033702d0bddece0f82d9
stw4114 <stw4114@nyu.edu>
2025-05-23 16:10:06 -0400

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -28,7 +28,7 @@ def welcome_assignment_answers(question):
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+        answer = "Error: Question not recognized. Please check for typos."
     return(answer)
 # Complete all the questions.
 
