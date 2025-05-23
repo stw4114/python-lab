a01481ad64ee221218247b37a11ef03b8ab1f14b
stw4114 <stw4114@nyu.edu>
2025-05-23 16:03:42 -0400

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -27,7 +27,7 @@ def welcome_assignment_answers(question):
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is not the answer you're looking for"
+        answer = "Once in a Lifetime"
     return answer
 # Complete all the questions.
 
