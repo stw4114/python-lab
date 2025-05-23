d9b43108e74e0c2c18c021bc2f26485e63e754d5
stw4114 <stw4114@nyu.edu>
2025-05-23 15:11:50 -0400

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -9,6 +9,22 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "db8a8e1b1014ed4e8cfb2c5c69e5b7d01df832e4f5d78f7a3efb0d3e1a03d229"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = "3"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = "2"
     else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
