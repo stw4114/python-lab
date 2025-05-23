792352066bcac0956fcfae7988b559d8f9c47357
stw4114 <stw4114@nyu.edu>
2025-05-23 03:23:40 -0400

    Add files via upload

---
+++ gettingStarted.py
@@ -0,0 +1,54 @@
+### welcome_assignment_answers
+### Input - All nine questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    # Students do not have to follow the skeleton for this assignment.
+    # Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "Yes"
+    else:
+        ### you should understand why this else case should be included
+        ### what happens if there is a typo in one of the questions?
+        ### maybe put something here to flag an issue and catch errors
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    return (answer)
+
+
+# Complete all the questions.
+
+
+if __name__ == "__main__":
+    # use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
+
+
+# Questions:
+# "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+# "Are encoding and encryption the same? - Yes/No":
+# "Is it possible to decrypt a message without a key? - Yes/No":
+# "Is it possible to decode a message without a key? - Yes/No":
+# "Is a hashed message supposed to be un-hashed? - Yes/No":
+# "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+# "Is MD5 a secured hashing algorithm? - Yes/No":
+# "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+# "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
